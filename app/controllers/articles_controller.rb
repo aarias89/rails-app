f1fb15b7606e9761d2c72cab40fba6389c8ff723
Andres Arias <andresar11@gmail.com>
2018-05-23 16:51:34 -0400

    Article creation process complete, no error on article creation Rspec

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,21 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    #create using strong params, this is a private method in order to whitelist info that it gets from the web.
+    @article = Article.new(article_params)
+    @article.save
+    #If successful save
+    flash[:success] = "Article has been created"
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:article, :body)
+  end
+
+
 end
