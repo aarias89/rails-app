2a06240ecdd2758a5a28d8500311d485bcea7d12
Andres Arias <andresar11@gmail.com>
2018-05-25 18:52:00 -0400

    Implement display message when no articles present

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -7,7 +7,6 @@ RSpec.feature "Listing Articles" do
     @article2 = Article.create(title: "Second test article", body: "Lorem Ipsum, I know no Latin")
   end
 
-
   scenario "A user lists all articles" do
     visit "/"
 
@@ -21,5 +20,27 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_link(@article2.title)
 
   end
+
+  scenario "A user has no artices" do
+    Article.delete_all
+
+    visit "/"
+
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+
+    within ("h1#no-articles") do
+      expect(page).to have_content("No Articles Created")
+    end
+
+  end
+
+
 
 end
