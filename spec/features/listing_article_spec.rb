bb19c1d275b56f004adf2f4c0e3c2f6493c7fa42
Andres Arias <andresar11@gmail.com>
2018-05-29 07:52:38 -0400

    New article button will appear only for logged in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature "Listing Articles" do
 
   before do
-    Bilbo = User.create(email: "baggins@example.com", password: "password")
-    @article1 = Article.create(title: "First test article", body:" Lorem Ipsum", user: Bilbo)
-    @article2 = Article.create(title: "Second test article", body: "Lorem Ipsum, I know no Latin", user: Bilbo)
+    @Bilbo = User.create(email: "baggins@example.com", password: "password")
+    @article1 = Article.create(title: "First test article", body:" Lorem Ipsum", user: @Bilbo)
+    @article2 = Article.create(title: "Second test article", body: "Lorem Ipsum, I know no Latin", user: @Bilbo)
   end
 
   scenario "with articles created and user not signed in" do
@@ -21,6 +21,23 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_link(@article2.title)
 
     expect(page).not_to have_link("New Article")
+
+  end
+
+  scenario "with articles created and user signed in" do
+    login_as(@Bilbo)
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+
+    expect(page).to have_link("New Article")
 
   end
 
