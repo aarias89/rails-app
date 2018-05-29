ffa61382f70b63d204318cb0fb6739c837c42a52
Andres Arias <andresar11@gmail.com>
2018-05-28 23:08:27 -0400

    Refactor code in article controller, all tests PASS

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @articles =  Article.all
   end
@@ -20,15 +23,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article =  Article.find(params[:id])
   end
 
   def edit
-    @article =  Article.find(params[:id])
   end
 
   def update
-    @article =  Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = "Article has been updated"
       redirect_to @article
@@ -39,7 +39,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:success] = "Article has been deleted"
       redirect_to articles_path
@@ -48,6 +47,7 @@ class ArticlesController < ApplicationController
 
   protected
 
+#defined at application_controller.rb
   def resource_not_found
     message = "The article you are looking for could not be found"
     flash[:alert] = message
@@ -55,6 +55,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def set_article
+    @article =  Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :body)
